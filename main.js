1720ef0de8ff0f384969aec824fa034a8fff1858
jTuokkola <jaakko.tuokkola@gmail.com>
2022-11-10 13:42:07 +0200

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function onclick(){
+let body = document.querySelector("body");
+let title = document.querySelector("h1");
+body.classList.toggle("drktheme");
+title.classList.toggle("titledrk");
+}
