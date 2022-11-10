c90dcc13999699d1e619156875ed57a019867b24
jTuokkola <jaakko.tuokkola@gmail.com>
2022-11-10 14:04:45 +0200

    rep

---
+++ rep.js
@@ -0,0 +1,9 @@
+
+function on_click(){
+let body = document.querySelector("body");
+let title = document.querySelector("h1");
+let p = document.querySelector("p");
+body.classList.toggle("drktheme");
+title.classList.toggle("titledrk");
+p.classList.add("textdrk");
+}
