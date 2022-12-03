5e9bc65661857f41a381b0e41ae2cb7b0fb169f4
jTuokkola <jaakko.tuokkola@gmail.com>
2022-12-03 14:56:09 +0200

    Turhan tiedoston poisto

--- rep/rep.js
+++
@@ -1,9 +0,0 @@
-
-function on_click(){
-let body = document.querySelector("body");
-let title = document.querySelector("h1");
-let p = document.querySelector("p");
-body.classList.toggle("drktheme");
-title.classList.toggle("titledrk");
-p.classList.add("textdrk");
-}
